e97f198bf64ab5b7635e947d6836b3b7b0a28f05
Ronin Evans <roninevans16@gmail.com>
2024-11-19 17:57:13 -0700

    creation of vehicle class with class lvl attributes

---
+++ Vehicle.py
@@ -0,0 +1,7 @@
+#Parent Class
+class Vehicle:
+    wheels = 4 #class lvl attribute assigned to all objects from the class
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
